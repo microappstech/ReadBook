ecca4dfb4db5259b7c457aa4d12c052ad4cbca4b
Hamza <hamzamouddakur@gmail.com>
2023-05-29 20:33:45 +0100

    New branch for project ServreBlazor readbook

--- ReadBook/Data/IServices/IUpload.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace ReadBook.Data.IServices
-{
-    public interface IUpload
-    {
-        public Task<bool> UploadCover(IFormFile file);
-        public Task UploadPicture(IFormFile picture);
-    }
-}
